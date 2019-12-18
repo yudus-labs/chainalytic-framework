95a5e52d545c27d6dbaf755f6eb898022e3170b4
duyyudus <duyyudus@gmail.com>
2019-12-18 17:29:36 +0700

    Better test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,15 @@ import shutil
 import pytest
 from chainalytic.common import config
 
+CHAINALYTIC_TEST = f'{config.CHAINALYTIC_FOLDER}_test'
+
 
 @pytest.fixture(scope='session')
 def setup_chainalytic_config(request):
     print('Setting up temp chainalytic data')
 
     cur_chainalytic_folder = config.CHAINALYTIC_FOLDER
-    config.CHAINALYTIC_FOLDER = f'{config.CHAINALYTIC_FOLDER}_test'
+    config.CHAINALYTIC_FOLDER = CHAINALYTIC_TEST
 
     cfg = config.init_user_config()
     print('Generated user config for testing')
@@ -28,8 +30,7 @@ def setup_chainalytic_config(request):
 def setup_temp_db(request):
     print('Setting up temp leveldb')
 
-    cur_chainalytic_folder = config.CHAINALYTIC_FOLDER
-    config.CHAINALYTIC_FOLDER = f'{config.CHAINALYTIC_FOLDER}_test'
+    test_chainalytic_folder = CHAINALYTIC_TEST
 
     db_path = Path(config.CHAINALYTIC_FOLDER).resolve().joinpath('TMP_DB')
     db_path.mkdir(parents=1, exist_ok=1)
@@ -37,9 +38,8 @@ def setup_temp_db(request):
     yield db_path.as_posix()
 
     def teardown():
-        test_chainalytic_data = Path(config.get_working_dir(), config.CHAINALYTIC_FOLDER,)
+        test_chainalytic_data = Path(config.get_working_dir(), test_chainalytic_folder)
         shutil.rmtree(test_chainalytic_data.as_posix(), ignore_errors=1)
-        config.CHAINALYTIC_FOLDER = cur_chainalytic_folder
         print('Deleted temp leveldb after testing')
 
     request.addfinalizer(teardown)
