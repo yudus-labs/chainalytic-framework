0c1f33b8a0e2302a0da639dbffcf06379e9fa24d
duyyudus <duyyudus@gmail.com>
2019-12-31 16:39:25 +0700

    Update tests

--- tests/common/test_config.py
+++ tests/common/test_config.py
@@ -12,14 +12,17 @@ def test_chain_registry_value(setup_chainalytic_config):
     assert 'public-icon' in zones
     z = zones['public-icon']
     assert z['zone_name'] == 'Public ICON mainnet'
-    assert z['client_endpoint'] == 'https://ctz.solidwallet.io'
+    assert z['client_endpoint'] == 'localhost:9000'
     assert z['chain_db_dir'] == ''
+    assert z['score_db_icondex_dir'] == ''
+    assert z['direct_db_access'] == 0
 
     assert 'public-ethereum' in zones
     z = zones['public-ethereum']
     assert z['zone_name'] == 'Public Ethereum mainnet'
     assert z['client_endpoint'] == ''
     assert z['chain_db_dir'] == ''
+    assert z['direct_db_access'] == 0
 
 
 def test_setting_value(setup_chainalytic_config):
@@ -30,17 +33,20 @@ def test_setting_value(setup_chainalytic_config):
         'aggregator_endpoint',
         'warehouse_endpoint',
         'provider_endpoint',
-        'warehouse_root',
-        'zone_storage_root',
+        'warehouse_dir',
+        'zone_storage_dir',
+        'transform_storage_dir',
+        'transform_cache_dir',
     ]
     for k in valid_keys:
         assert k in setting
 
-    assert setting['aggregator_endpoint'] == 'localhost:5500'
+    assert setting['upstream_endpoint'] == 'localhost:5500'
     assert setting['aggregator_endpoint'] == 'localhost:5510'
     assert setting['warehouse_endpoint'] == 'localhost:5520'
     assert setting['provider_endpoint'] == 'localhost:5530'
     assert setting['warehouse_dir'] == '.chainalytic/chainalytic_warehouse'
     assert setting['zone_storage_dir'] == '{warehouse_dir}/{zone_id}_storage'
     assert setting['transform_storage_dir'] == '{zone_storage_dir}/{transform_id}_storage'
+    assert setting['transform_cache_dir'] == '{zone_storage_dir}/{transform_id}_cache'
 
--- tests/hub/test_hub_main.py
+++
@@ -1,7 +0,0 @@
-from chainalytic.hub import Launcher
-
-
-def test_launcher():
-    ret = Launcher().launch()
-    assert ret == 1
-    Launcher().clean()
