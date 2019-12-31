48f700f65da2b4b9a484325ccc41919f598d3c29
duyyudus <duyyudus@gmail.com>
2019-12-31 14:23:47 +0700

    Better arguments

--- launch.py
+++ launch.py
@@ -13,14 +13,17 @@ if __name__ == '__main__':
     )
     parser.add_argument('-i', '--init-config', action='store_true', help='Generate user config')
     parser.add_argument('--restart', action='store_true', help='Force restart all running services')
-    parser.add_argument('-t', '--transform-id', help='Monitor specific transform. Default is "stake_history"')
-    parser.add_argument('-r', '--refresh-time', help='Refresh time of aggregation monitor')
 
-    subparsers = parser.add_subparsers(dest='command')
+    subparsers = parser.add_subparsers(dest='command', help='Sub commands')
     stop_parser = subparsers.add_parser('stop', help='Kill running Chainalytic services')
     stop_parser.add_argument(
         'sid', nargs='?', default=None, help='Service ID, kill specific service'
     )
+    monitor_parser = subparsers.add_parser('m', help='Monitor one specific transform')
+    monitor_parser.add_argument(
+        'transform_id', nargs='?', default=None, help='Transform ID',
+    )
+    monitor_parser.add_argument('-r', '--refresh-time', help='Refresh time of aggregation monitor')
 
     args = parser.parse_args()
     console = Console()
@@ -29,16 +32,17 @@ if __name__ == '__main__':
         if args.command == 'stop':
             console.load_config()
             console.stop_services(args.sid)
+        elif args.command == 'm':
+            console.load_config()
+            console.monitor(
+                args.transform_id if args.transform_id else 'stake_history',
+                float(args.refresh_time) if args.refresh_time else 1,
+            )
         elif args.init_config:
             console.init_config()
         else:
             console.load_config()
             console.init_services(service_id=args.sid, force_restart=args.restart)
-            if not args.sid:
-                console.monitor(
-                    args.transform_id if args.transform_id else 'stake_history',
-                    float(args.refresh_time) if args.refresh_time else 1,
-                )
     except KeyboardInterrupt:
         print('Exited Chainalytic Console')
         sys.exit()
