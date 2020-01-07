50a904b5bf2a71380b785e28dbef1b3aa321dae9
duyyudus <duyyudus@gmail.com>
2020-01-07 08:19:44 +0000

    Fix bug

--- src/chainalytic/zones/public-icon/aggregator/transform_registry/abstention_stake.py
+++ src/chainalytic/zones/public-icon/aggregator/transform_registry/abstention_stake.py
@@ -1,6 +1,7 @@
 import json
 import time
 from typing import Dict, List, Optional, Set, Tuple, Union
+import traceback
 
 import plyvel
 from iconservice.icon_config import default_icon_config
@@ -13,13 +14,12 @@ from chainalytic.common import rpc_client, trie
 
 class Transform(BaseTransform):
     LAST_STATE_HEIGHT_KEY = b'last_state_height'
+    MAX_WALLETS = 200
 
     def __init__(self, working_dir: str, zone_id: str, transform_id: str):
         super(Transform, self).__init__(working_dir, zone_id, transform_id)
 
     async def execute(self, height: int, input_data: dict) -> Optional[Dict]:
-        start_time = time.time()
-
         # Load transform cache to retrive previous staking state
         cache_db = self.transform_cache_db
         cache_db_batch = self.transform_cache_db.write_batch()
@@ -63,7 +63,7 @@ class Transform(BaseTransform):
 
             addr_data = f'{stake}:{delegation}'
 
-            if stake - delegation > 1:
+            if stake - delegation > 1 and stake > 0:
                 abstention_stake[addr] = addr_data
             elif addr in abstention_stake:
                 abstention_stake.pop(addr)
@@ -74,10 +74,16 @@ class Transform(BaseTransform):
         for addr, targets in set_delegation_wallets.items():
             delegation_val = 0
             for t in targets:
+                # delegation_val += int(t['value'], 16) / 10 ** 18
                 try:
-                    delegation_val += float(t['value'], 16) / 10 ** 18
-                except Exception:
-                    pass
+                    delegation_val += int(t['value'], 16) / 10 ** 18
+                except Exception as e:
+                    print(f'{e}\n{traceback.format_exc()}')
+                    delegation_val = None
+                    break
+
+            if delegation_val is None:
+                continue
 
             addr_data = cache_db.get(addr.encode())
             if addr_data:
@@ -90,18 +96,29 @@ class Transform(BaseTransform):
 
             addr_data = f'{stake}:{delegation}'
 
-            if stake - delegation > 1:
+            if stake - delegation > 1 and stake > 0:
                 abstention_stake[addr] = addr_data
             elif addr in abstention_stake:
                 abstention_stake.pop(addr)
 
             cache_db_batch.put(addr.encode(), addr_data.encode())
 
+        if state_changed:
+            abstention_stake = {
+                k: v
+                for k, v in sorted(
+                    abstention_stake.items(),
+                    key=lambda item: float(item[1].split(':')[0]),
+                    reverse=1,
+                )
+            }
+            if len(abstention_stake) > Transform.MAX_WALLETS:
+                top_addresses = list(abstention_stake)[: Transform.MAX_WALLETS]
+                abstention_stake = {k: v for k, v in abstention_stake.items() if k in top_addresses}
+
         cache_db_batch.put(b'abstention_stake', json.dumps(abstention_stake).encode())
         cache_db_batch.put(Transform.LAST_STATE_HEIGHT_KEY, str(height).encode())
         cache_db_batch.write()
-
-        # execution_time = f'{round(time.time()-start_time, 4)}s'
 
         return {
             'height': height,
